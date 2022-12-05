83ac3da09b4ad56277575142e93455a105fa88ca
Barbaros YILDIRIM <barbarosayildirim@gmail.com>
2022-12-05 22:19:08 +0300

    feat: day 4 part 1 solution

---
+++ src/Day4/Day4.ts
@@ -0,0 +1,19 @@
+import { input } from "./input";
+
+const lines : string[] = input.split("\n");
+const pairs : string[][] = lines.map((line) => line.split(","));
+
+let fullyContainingPairs : number = 0;
+
+pairs.forEach((pair) => {
+    const pair1 : number[] = pair[0].split("-").map((num) => parseInt(num));
+    const pair2 : number[] = pair[1].split("-").map((num) => parseInt(num));
+
+    if (pair1[0] <= pair2[0] && pair1[1] >= pair2[1]) {
+        fullyContainingPairs++;
+    } else if (pair2[0] <= pair1[0] && pair2[1] >= pair1[1]) {
+        fullyContainingPairs++;
+    } 
+})
+
+console.log("Solution 4-1", fullyContainingPairs);
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { firstDayResults } from './Day1/AoC_1';
-import { secondDayResults } from './Day2/AoC_2';
-
-console.log("AoC Results: ");
-
-firstDayResults();
-secondDayResults();
