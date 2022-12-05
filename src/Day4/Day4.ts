ffd7b4a9b3a3411e77ac5e000fce9d6438c9c543
Barbaros YILDIRIM <barbarosayildirim@gmail.com>
2022-12-05 22:24:39 +0300

    feat: day 4 part 2 solution

--- src/Day4/Day4.ts
+++ src/Day4/Day4.ts
@@ -4,6 +4,7 @@ const lines : string[] = input.split("\n");
 const pairs : string[][] = lines.map((line) => line.split(","));
 
 let fullyContainingPairs : number = 0;
+let overlappingPairs : number = 0;
 
 pairs.forEach((pair) => {
     const pair1 : number[] = pair[0].split("-").map((num) => parseInt(num));
@@ -16,4 +17,16 @@ pairs.forEach((pair) => {
     } 
 })
 
+pairs.forEach((pair) => {
+    const pair1 : number[] = pair[0].split("-").map((num) => parseInt(num));
+    const pair2 : number[] = pair[1].split("-").map((num) => parseInt(num));
+
+    if (pair1[0] <= pair2[0] && pair1[1] >= pair2[0]) {
+        overlappingPairs++;
+    } else if (pair2[0] <= pair1[0] && pair2[1] >= pair1[0]) {
+        overlappingPairs++;
+    }
+})
+
 console.log("Solution 4-1", fullyContainingPairs);
+console.log("Solution 4-2", overlappingPairs);
